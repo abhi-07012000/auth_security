dbd2bc498b1c76ed4b8a396566d08f0112391af1
Abhijeet mane <abhi.mane07012000@gmail.com>
2022-11-04 16:58:19 +0530

    bcrypt Done

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose= require("mongoose")
 const encrypt= require("mongoose-encryption");
-const md5= require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -47,8 +48,12 @@ app.route("/login")
             console.log(err);
         }
         else{
-            if(foundUser.password===md5(password))
-                res.render("secrets");
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result == true){
+                    res.render("secrets");
+                }
+            });
+                
         }
 
     })
@@ -65,19 +70,23 @@ app.route("/register")
 
 .post(function (req,res) {
 
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    }); 
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+        }); 
+    
+        newUser.save(function (err) {
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });
+    });
 
-    newUser.save(function (err) {
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    })
+    
 
 
 });
